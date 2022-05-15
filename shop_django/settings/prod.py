321b3229ff85c28ceddc3a6b4b65e5ef036b361f
Jaesik <joje1283@gmail.com>
2022-05-16 00:17:08 +0900

    주석 추가

--- shop_django/settings/prod.py
+++ shop_django/settings/prod.py
@@ -1,4 +1,6 @@
 from .common import *
 
-ALLOWED_HOSTS = ["api.joje.link", "127.0.0.1"]
+# Nginx가 프록시 서버 역할을 하고있고, http://127.0.0.1:8000으로 리디렉션 하고있기에,
+# 허용 API를 127.0.0.1로 설정함.
+ALLOWED_HOSTS = ["127.0.0.1"]
 DEBUG = False
