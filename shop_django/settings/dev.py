35e443bb3132a7b54a482177480105f5ca4b1714
Jaesik <joje1283@gmail.com>
2022-05-18 01:11:08 +0900

    postgresql 설정 셋업 배포 스크립트에 변경 사항 반영

--- shop_django/settings/dev.py
+++ shop_django/settings/dev.py
@@ -1,16 +1,11 @@
 from .common import *
 
-# Nginx가 프록시 서버 역할을 하고있고, http://127.0.0.1:8000으로 리디렉션 하고있기에,
-# 허용 API를 127.0.0.1로 설정함.
-ALLOWED_HOSTS = ["127.0.0.1"]
-DEBUG = False
-
-STATIC_ROOT = os.getenv("STATIC_ROOT")
+DEBUG = True
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'shop_api_django',
+        'NAME': 'shop_api_django_dev',
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
