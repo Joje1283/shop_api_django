04c295ed51efd307c32fae00fe33752d571c4373
Jaesik <joje1283@gmail.com>
2022-05-18 02:36:41 +0900

    오타 수정

--- shop_django/settings/prod.py
+++ shop_django/settings/prod.py
@@ -3,7 +3,7 @@ from .common import *
 # Nginx가 프록시 서버 역할을 하고있고, http://127.0.0.1:8000으로 리디렉션 하고있기에,
 # 허용 API를 127.0.0.1로 설정함.
 ALLOWED_HOSTS = ["127.0.0.1"]
-CSRF_TRUSTED_ORIGINS = ["https://*.joje.link", "http://127.0.0.1:8000", "http://127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ["https://*.joje.link"]
 DEBUG = True
 
 STATIC_ROOT = os.getenv("STATIC_ROOT")
