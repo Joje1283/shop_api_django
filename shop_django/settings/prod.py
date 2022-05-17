12772ba72982912720b69e9e1a44f66a44f01abc
Jaesik <joje1283@gmail.com>
2022-05-18 02:43:23 +0900

    주석 추가

--- shop_django/settings/prod.py
+++ shop_django/settings/prod.py
@@ -3,10 +3,10 @@ from .common import *
 # Nginx가 프록시 서버 역할을 하고있고, http://127.0.0.1:8000으로 리디렉션 하고있기에,
 # 허용 API를 127.0.0.1로 설정함.
 ALLOWED_HOSTS = ["127.0.0.1"]
-CSRF_TRUSTED_ORIGINS = ["https://*.joje.link"]
+CSRF_TRUSTED_ORIGINS = ["https://*.joje.link"]  # 안전하지 않은 요청에 대한 신뢰할 수 있는 출처 목록 (CSRF 오류로 인해 설정)
 DEBUG = False
 
-STATIC_ROOT = os.getenv("STATIC_ROOT")
+STATIC_ROOT = os.getenv("STATIC_ROOT")  # 배포서버에서 입력받음 (/var/www/staticfiles)
 
 DATABASES = {
     'default': {
