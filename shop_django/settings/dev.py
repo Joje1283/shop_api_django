151e59c8ab566e9e68ca1bfc960ddefe20625d8a
Jaesik <joje1283@gmail.com>
2022-05-21 15:41:37 +0900

    테스트 수행 시 in-memory DB를 사용하도록 수정

--- shop_django/settings/dev.py
+++ shop_django/settings/dev.py
@@ -1,17 +1,21 @@
+import sys
+
 from .common import *
 
 DEBUG = True
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'shop_api_django_dev',
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
-        'TEST': {
-            'NAME': 'test_db',
+if 'test' in sys.argv:
+    DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3'}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'shop_api_django_dev',
+            'USER': os.getenv("DB_USER"),
+            'PASSWORD': os.getenv("DB_PASSWORD"),
+            'HOST': os.getenv("DB_HOST"),
+            'PORT': os.getenv("DB_PORT"),
+            'TEST': {
+                'NAME': 'test_db',
+            }
         }
     }
-}
