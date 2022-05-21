9c58d038ae7f436cc64762c43348e9f09b01f318
Jaesik <joje1283@gmail.com>
2022-05-21 15:11:49 +0900

    회원 비즈니스 로직 개발

---
+++ members/exceptions.py
@@ -0,0 +1,2 @@
+class MemberException(Exception):
+    pass
--- members/models.py
+++ members/models.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
+
+from .exceptions import MemberException
 
 
 class Address(models.Model):
@@ -11,6 +13,26 @@ class Address(models.Model):
         abstract = True
 
 
+class MemberManager(UserManager):
+    def join(self, password, username, **kwargs) -> int:
+        """
+        회원 가입
+        """
+        self.validate_duplicate_member(username=username)
+        member = self.create_user(username=username, **kwargs)
+        member.set_password(password)
+        return member.pk
+
+    def validate_duplicate_member(self, username: str) -> None:
+        """
+        중복 회원 검증
+        """
+        if self.filter(username=username).exists():
+            raise MemberException("이미 존재하는 회원입니다.")
+
+
 class Member(AbstractUser, Address):
     def __str__(self):
         return self.username
+
+    objects = MemberManager()
